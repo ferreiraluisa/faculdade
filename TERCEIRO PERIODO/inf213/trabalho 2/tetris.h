53b068b61089577cd0d5adfa61c81a7c8eed9fb4
luisa <luisa.ferreira@ufv.br>
2021-09-08 23:54:00 -0300

    criando classe tetris

--- TERCEIRO PERIODO/inf213/trabalho 2/tetris.h
+++ TERCEIRO PERIODO/inf213/trabalho 2/tetris.h
@@ -1,1 +1,18 @@
 #ifndef TETRIS_H
+#define TETRIS_H
+
+class Tetris{
+    private:
+        int numColums;
+    public:
+        Tetris(int _numColums);
+        char get(int colums, int lines);
+        void removeColuna(int coluna);
+        void removeLinhasCompletas(int linha);
+        void getNumColum();
+        void getAltura(int colum);
+        void getAltura();
+        void adicionaForma(int coluna, int linha, char id, int rotacao);
+};
+
+#endif
