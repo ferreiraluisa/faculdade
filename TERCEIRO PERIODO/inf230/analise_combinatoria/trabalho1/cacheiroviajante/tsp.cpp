3be5a37c5a529b85e7adb609c13d5841ac16effc
luisa <luisa.ferreira@ufv.br>
2021-10-18 19:11:25 -0300

    cacheiro viajante final

--- TERCEIRO PERIODO/inf230/analise_combinatoria/trabalho1/cacheiroviajante/tsp.cpp
+++ TERCEIRO PERIODO/inf230/analise_combinatoria/trabalho1/cacheiroviajante/tsp.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <fstream>
 #include <utility>
+#include <sstream>
 using namespace std;
 
 //essa funcao gera todas as permutacoes de 1 ate n
-void geraPermutacao(int vetor[],vector<vector<int>>graph, int inicio, int tamanho,int *menor)
+void geraPermutacao(int vetor[],vector<vector<int>>graph,vector<int> &melhorRota, int inicio, int tamanho,int *menor)
 {
     int distancia=0;
 	if(inicio == tamanho)
@@ -16,14 +18,19 @@ void geraPermutacao(int vetor[],vector<vector<int>>graph, int inicio, int tamanh
             if(i == tamanho) distancia += graph[vetor[i]][vetor[0]];
             else distancia+= graph[vetor[i]][vetor[i+1]];
         }
-        if(distancia < *menor && distancia > 0) *menor = distancia;
+        if(distancia < *menor && distancia > 0) {
+            *menor = distancia;
+            melhorRota = {};
+            for(int i=0;i<=tamanho;i++) melhorRota.push_back(vetor[i]);
+            melhorRota.push_back(vetor[0]);
+        }
 	}
 	else
 	{
 		for(int i = inicio; i <= tamanho; i++)
 		{
 			swap(vetor[inicio], vetor[i]);
-			geraPermutacao(vetor,graph, inicio + 1, tamanho, menor);
+			geraPermutacao(vetor,graph,melhorRota, inicio + 1, tamanho, menor);
 			swap(vetor[inicio], vetor[i]);// backtracking
 		}
 	}
@@ -33,41 +40,49 @@ int main(int argc, char const *argv[])
 {
     vector<int> auxVec;
     vector<vector<int>> graph;
-    int qtdeCidades = 4;
-    int v[4] = {0,1,2,3};
-    auxVec.push_back(0);
-    auxVec.push_back(7);
-    auxVec.push_back(8);
-    auxVec.push_back(7);
-    graph.push_back(auxVec);
-    auxVec = {};
-    auxVec.push_back(5);
-    auxVec.push_back(0);
-    auxVec.push_back(8);
-    auxVec.push_back(8);
-    graph.push_back(auxVec);
-    auxVec = {};
-    auxVec.push_back(9);
-    auxVec.push_back(6);
-    auxVec.push_back(0);
-    auxVec.push_back(9);
-    graph.push_back(auxVec);
-    auxVec = {};
-    auxVec.push_back(10);
-    auxVec.push_back(7);
-    auxVec.push_back(10);
-    auxVec.push_back(0);
-    graph.push_back(auxVec);
+    vector<int> melhorRota;
+    int qtdeCidades;
+    int cont = 0;
+    int menor = 9999999;
 
-    // for(int i=0;i<4;i++){
-    //     for(int j=0;j<4;j++){
+    ifstream file(argv[1]);
+    if (file.is_open()){
+        string line;
+        while (! file.eof()){ 
+            getline(file,line); 
+            if(cont == 0) {
+                qtdeCidades = stoi(line);
+            }
+            else{
+                stringstream ss;
+                ss << line;
+                int aux[qtdeCidades];
+                for(int i=0;i<qtdeCidades;i++) ss >> aux[i];
+                for(int i=0;i<qtdeCidades;i++) auxVec.push_back(aux[i]);
+                graph.push_back(auxVec);
+                auxVec = {};
+            }
+            cont++;
+        }
+        file.close();
+    }
+
+    int v[qtdeCidades];
+    for(int i=0;i<qtdeCidades;i++) v[i] = i;
+    
+
+    // for(int i=0;i<qtdeCidades;i++){
+    //     for(int j=0;j<qtdeCidades;j++){
     //         cout<<graph[i][j]<<" ";
     //     }
     //     cout<<endl;
     // }
-    int menor;
-    geraPermutacao(v,graph,0,3,&menor);
+    geraPermutacao(v,graph,melhorRota,0,qtdeCidades-1,&menor);
 
-    cout<<menor<<endl;
+    for(int i=0;i<melhorRota.size();i++){
+        if(i == melhorRota.size() - 1) cout<<melhorRota[i]<<endl;
+        else cout<<melhorRota[i]<<" -> ";
+    }
+    cout<<"MENOR DISTANCIA: "<<menor<<endl;
     return 0;
 }
